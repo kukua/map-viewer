2a1eb2cc89b028971ff337b2df10268f1ddcb189
Siebren Kranenburg <info@siebrenkranenburg.nl>
2016-06-30 12:21:22 +0200

    Added .env file (instead of dbinfo) & fixed the XML output since the SimpleXMLElement class doesn't exist in this PHP version

--- dbinfo.php
+++
@@ -1,5 +0,0 @@
-<?php
-$username="admin";
-$password="Mosesbyanyuma109";
-$database="admin-moses";
-?>
--- outputXML.php
+++ outputXML.php
@@ -1,26 +1,33 @@
 <?php
 #Programmer: Moses Byanyuma
 
-require(__DIR__.'/dbinfo.php');
+require('./.env');
 
 // Opens a connection to a mySQL server
 try {
-$db = new PDO('mysql:host=moses.kukua.tech;dbname=moses;charset=utf8',  'admin', 'Mosesbyanyuma109');
-   // echo "Connected to the database"; //checking for connection
-}
-catch (PDOException $e)
-{
+	$db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME. ';charset=utf8',  DB_USERNAME, DB_PASSWORD);
+} catch (PDOException $e) {
     echo "An Error occured, could not connect!";
 }
 
+$statement = $db->query('SELECT * FROM markers');
+//$result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-$statement = $db->query('SELECT * FROM markers WHERE 1');
-//$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$str = <<<XML
+<xml>
+       <marker name="test"   lat="-4.8199534" lng="38.35222" address="Test street 1" type="1"></marker>
+       <marker name="test 2" lat="-4.8199534" lng="38.34222" address="Test street 2" type="1"></marker>
+</xml>
+XML;
 
-// Build XML
+header("Content-type: text/xml");
+echo $str;
+exit;
+
+/**
 $xml = new SimpleXMLElement('<xml/>');
 
-while ($row = $statement->fetch(PDO::FETCH_ASSOC)) { 
+while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
 	$node = $xml->addChild('marker');
 	$node->addAttribute('name', $row['name']);
 	$node->addAttribute('address', $row['address']);
@@ -32,4 +39,4 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
 header('Content-type: text/xml');
 print($xml->asXML());
 exit;
-?>
+*/
